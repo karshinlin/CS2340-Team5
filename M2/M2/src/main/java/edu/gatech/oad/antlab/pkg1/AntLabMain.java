428397f85a2e47b1da9aa056a538d0709366370f
allyliu <allyliu@github.com>
2017-09-06 14:12:29 -0400

    added p5 and readme

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,10 +71,10 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
                   toPrint += p4.toString("gburdell4");
-          //Person5 replace P4 with your name
-          //and gburdell5 with your gt id
-          Person5 p5 = new Person5("Ally Liu");
-          toPrint += p5.toString("sliu402");
+                  //Person5 replace P4 with your name
+                  //and gburdell5 with your gt id
+                  Person5 p5 = new Person5("Ally Liu");
+                  toPrint += p5.toString("sliu402");
 
         System.out.println(toPrint);
 
