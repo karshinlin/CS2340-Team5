614729296a10865e3030ff52be3f072be45efcd4
maxengle <maxengle@yahoo.com>
2017-11-01 03:10:17 -0400

    Set date and count arrays

--- Ratastic/app/src/main/java/edu/gatech/teamraid/ratastic/GraphActivity.java
+++ Ratastic/app/src/main/java/edu/gatech/teamraid/ratastic/GraphActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
+import edu.gatech.teamraid.ratastic.Model.RatSighting;
+
 /**
  * Created by maxengle on 10/31/17.
  */
@@ -25,5 +29,22 @@ public class GraphActivity extends AppCompatActivity {
                 GraphActivity.this.startActivity(switchToMap);
             }
         });
+
+        String thisDate = "";
+        ArrayList<String> dateArray = new ArrayList<String>();
+        ArrayList<Integer> countArray = new ArrayList<Integer>();
+        for (RatSighting ratSight : RatSighting.ratSightingArray) {
+            thisDate = ratSight.getCreatedDate();
+            thisDate = thisDate.substring(0, 7);
+            if (dateArray.contains(thisDate)) {
+                int index = dateArray.indexOf(thisDate);
+                int curr = countArray.get(index);
+                curr++;
+                countArray.set(index, curr);
+            } else {
+                dateArray.add(thisDate);
+                countArray.add(1);
+            }
+        }
     }
 }
