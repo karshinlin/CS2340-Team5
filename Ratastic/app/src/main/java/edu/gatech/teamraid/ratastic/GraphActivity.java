c064d39e106b8103cf47c72c20b4109b37d33c64
maxengle <maxengle@yahoo.com>
2017-11-01 04:10:35 -0400

    Created the graph.

--- Ratastic/app/src/main/java/edu/gatech/teamraid/ratastic/GraphActivity.java
+++ Ratastic/app/src/main/java/edu/gatech/teamraid/ratastic/GraphActivity.java
@@ -6,6 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 
 import edu.gatech.teamraid.ratastic.Model.RatSighting;
@@ -15,6 +24,8 @@ import edu.gatech.teamraid.ratastic.Model.RatSighting;
  */
 
 public class GraphActivity extends AppCompatActivity {
+
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,10 @@ public class GraphActivity extends AppCompatActivity {
                 GraphActivity.this.startActivity(switchToMap);
             }
         });
+
+        barChart = (BarChart) findViewById(R.id.barChart);
+
+        barChart.setDrawGridBackground(true);
 
         String thisDate = "";
         ArrayList<String> dateArray = new ArrayList<String>();
@@ -45,6 +60,37 @@ public class GraphActivity extends AppCompatActivity {
                 dateArray.add(thisDate);
                 countArray.add(1);
             }
+        }
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        for (int i = 0; i < countArray.size(); i++) {
+            barEntries.add(new BarEntry(i, countArray.get(i)));
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Number of sightings for the specified" +
+                " date (Year-Month)");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData data = new BarData(barDataSet);
+        data.setBarWidth(0.9f);
+
+        barChart.setData(data);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new MyXAxisValueFormatter(dateArray));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        xAxis.setGranularity(1);
+    }
+
+    private class MyXAxisValueFormatter implements IAxisValueFormatter {
+        private ArrayList<String> dateValues;
+        private MyXAxisValueFormatter(ArrayList<String> values) {
+            this.dateValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return dateValues.get((int) value);
         }
     }
 }
